c95d801695e25b85fb64f69fd20bd352bdeb357b
Kastrcn <Kastrcn@WEIYOU-PRO>
2018-01-11 11:54:47 +0800

    添加项目文件。

---
+++ ASP.NETCOREIdentity/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCOREIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Placeholder", "Placeholder");
+            return View(data);
+        }
+
+
+    }
+}
